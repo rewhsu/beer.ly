f9e0c092ffdeec48a2937592841eae05079cfa93
Andrew Hsu <andrewhsu42@gmail.com>
2017-02-07 19:15:41 -0800

    fixed untracked files

--- server/config/apiKeys.js
+++
@@ -1,9 +0,0 @@
-module.exports.breweryDBKey = '423fbf870459d02e9ddbbca52269f481';
-
-module.exports.googleMapsAPIKey = 'AIzaSyCdY261c-7KYKKY46H8-t-eSxyx-I9z3Vc';
-
-module.exports.stripeTestKey = 'sk_test_RrR8vf0PJN14OTZk5GTtv3wz';
-
-module.exports.stripePublishableKey = 'pk_test_8uXUMefhx0DvWNbN78SnO73O';
-
-module.exports.beerlyAuthSecret = 'Little secret, big responsibility...';
