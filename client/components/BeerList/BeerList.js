41897bf2e9acec3ec82709ec0982af76a1cb2bde
Andrew Hsu <andrewhsu42@gmail.com>
2017-02-09 10:17:05 -0800

    Refactor BeerList to stateful class component

--- client/components/BeerList/BeerList.js
+++ client/components/BeerList/BeerList.js
@@ -2,19 +2,26 @@ import React, { PropTypes } from 'react';
 import BeerItem from '../BeerItem/BeerItem';
 import styles from './BeerList.css';
 
-const BeerList = (props) => {
-  const input = props.filter.toLowerCase();
-  const beers = props.beers.map((beer) => {
-    if (!input || !!beer.beer.beer_name.toLowerCase().includes(input)) {
-      return <BeerItem key={beer.beer.bid} beer={beer.beer} className={styles.beerItem} isBeingRenderedInCart={false} addToCart={props.addToCart} />;
-    }
-  });
+class BeerList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  return (
-	  <div className={styles.grid}>
-			{beers}
-	  </div>
-	);
+    }
+  }
+  render() {
+    const input = this.props.filter.toLowerCase();
+    const beers = this.props.beers.map((beer) => {
+      if (!input || !!beer.beer.beer_name.toLowerCase().includes(input)) {
+        return <BeerItem key={beer.beer.bid} beer={beer.beer} className={styles.beerItem} isBeingRenderedInCart={false} addToCart={this.props.addToCart} />;
+      }
+    });
+    return (
+      <div className={styles.grid}>
+        {beers}
+      </div>
+    );
+  }
 };
 
 BeerList.propTypes = {
